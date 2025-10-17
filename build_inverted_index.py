cef519b817b7262c9fd15f158b1240c120a7b27d
Ozel Yilmazel <ozel@yilmazel.com>
2025-10-17 13:55:29 +0000

    build index

---
+++ build_inverted_index.py
@@ -0,0 +1,74 @@
+import os
+import json
+import tqdm
+import orjson
+from pathlib import Path
+from util import extract_id, tokenize
+from collections import Counter
+
+if __name__ == "__main__":
+    dataset_path = Path("dataset")
+    splits = ["train", "val", "test"]
+
+    tracks_index = {}
+    for split in splits:
+        split_path = dataset_path / split
+        files = os.listdir(split_path)
+        split_avg_len = 0
+        for file in tqdm.tqdm(files, desc=f"Processing {split} split"):
+            file_path = split_path / file
+            with open(file_path, "r") as f:
+                data = orjson.loads(f.read())
+
+                for playlist in data["playlists"]:
+                    split_avg_len += len(playlist["tracks"])
+                    for track in playlist["tracks"]:
+                        track_id = extract_id(track["track_uri"])
+                        if track_id not in tracks_index:
+                            tracks_index[track_id] = track
+                            tracks_index[track_id]["extended_text"] = " ".join(
+                                tokenize(
+                                    track["track_name"]
+                                    + " "
+                                    + track["artist_name"]
+                                    + " "
+                                    + track["album_name"]
+                                )
+                            )
+                            del tracks_index[track_id]["pos"]
+
+                        if split == "train":
+                            extended_text = " ".join(
+                                tokenize(
+                                    playlist["name"]
+                                    + " "
+                                    + playlist.get("description", "")
+                                )
+                            )
+                            tracks_index[track_id]["extended_text"] += (
+                                " " + extended_text
+                            )
+
+        print(
+            f"Average number of playlists per file in {split} split: {split_avg_len / len(files)}"
+        )
+
+    with open(dataset_path / "track_corpus.json", "w") as f:
+        json.dump(tracks_index, f, indent=2)
+    
+    inverted_index = {}
+    inverted_index["DOCID_MAP"] = []
+    inverted_index["TOTAL_DOCS"] = len(tracks_index)
+
+    for docid, (track_id, track) in tqdm.tqdm(enumerate(tracks_index.items())):
+        tokens = Counter(tokenize(track["extended_text"]))
+
+        inverted_index["DOCID_MAP"].append(track_id)
+        for token, tf in tokens.items():
+            if token not in inverted_index:
+                inverted_index[token] = list()
+            inverted_index[token].append((docid, tf))
+    
+    with open(dataset_path / "inverted_index.json", "w") as f:
+        json.dump(inverted_index, f, indent=2)
+    print(f"Inverted index built and saved. {len(inverted_index)-2} unique tokens, {inverted_index["TOTAL_DOCS"]} Documents indexed.")
